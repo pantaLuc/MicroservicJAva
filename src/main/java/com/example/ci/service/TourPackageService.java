27ffa7f44922d7661249e280fac60c9c3f160337
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-10-01 14:29:35 +0200

    adding tourService

--- src/main/java/com/example/ci/service/TourPackageService.java
+++ src/main/java/com/example/ci/service/TourPackageService.java
@@ -1,7 +1,37 @@
 package com.example.ci.service;
 
+import com.example.ci.model.TourPackage;
+import com.example.ci.repository.TourPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TourPackageService {
+    private TourPackageRepository tourPackageRepository ;
+    @Autowired
+    public  TourPackageService(TourPackageRepository tourPackageRepository){
+        this.tourPackageRepository=tourPackageRepository;
+    }
+
+    /*
+    *  Create a tour Package
+    * */
+
+    public TourPackage createTourPackage(String code , String name){
+        return tourPackageRepository.findById(code).orElse(tourPackageRepository.save(new TourPackage(code ,name)));
+    }
+    /*Find
+    * Find all TourPackage Repository
+    * */
+    public Iterable<TourPackage> listTourPackage(){
+        return tourPackageRepository.findAll();
+    }
+
+    /*
+    * Total of tour Package
+    * */
+
+    public long totalTourpackage(){
+        return  tourPackageRepository.count();
+    }
 }
---
+++ src/main/java/com/example/ci/service/TourService.java
@@ -0,0 +1,4 @@
+package com.example.ci.service;
+
+public class TourService {
+}
