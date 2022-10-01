f157d8a7c51136df2f1d407132c5f95250667158
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-10-01 14:13:38 +0200

    first commit

---
+++ src/main/java/com/example/ci/repository/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.example.ci.repository;
+
+import com.example.ci.model.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage ,String> {
+}
